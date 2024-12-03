a3dc5449ba789ea7fa6bf25e98ce0ea5776aca0f
alankaohc <alanapplecat@gmail.com>
2024-12-03 18:21:05 +0800

    sphere frustum culling

--- src/shader/myComputeShader.glsl
+++ src/shader/myComputeShader.glsl
@@ -59,36 +59,63 @@ uniform mat4 viewProjMat;
 
 void main() {
 	const uint idx = gl_GlobalInvocationID.x;
-
+	vec4 center;
+	float radius;
+	int con_n;
 	if(idx >= numMaxInstance){ return; }
 
+	if (idx>=0 && idx < commands[1].baseInstance) {
+		center = vec4(0.0, 1.76, 0.0, 1.0);
+		radius = 2.6;
+		con_n = 0;
+	}
+	else if (idx>=commands[1].baseInstance && idx<commands[2].baseInstance) {
+		center = vec4(0.0, 4.57, 0.0, 1.0);
+		radius = 10.2;
+		con_n = 1;
+	}
+	else if (idx>=commands[2].baseInstance && idx<commands[3].baseInstance) {
+		center = vec4(0.0, 4.57, 0.0, 1.0);
+		radius = 8.5;
+		con_n = 2;
+	}
+	else if (idx>=commands[3].baseInstance && idx<commands[4].baseInstance) {
+		center = vec4(0.0, 2.55, 0.0, 1.0);
+		radius = 3.4;
+		con_n = 3;
+	}
+	else if (idx>=commands[4].baseInstance) {
+		center = vec4(0.0, 0.66, 0.0, 1.0);
+		radius = 1.4;
+		con_n = 4;
+	}
+	center.xyz = center.xyz + rawInstanceProps[idx].position.xyz;
+	vec4 v_left = center + vec4(-radius, 0, 0, 1);
+	vec4 v_right = center + vec4(radius, 0, 0, 1);
+	vec4 v_top = center + vec4(0, radius, 0, 1);
+	vec4 v_bottom = center + vec4(0, -radius, 0, 1);
+	vec4 v_near = center + vec4(0, 0, radius, 1);
+	vec4 v_far = center + vec4(0, 0, -radius, 1);
+	vec4 array[6];
+	array[0] = v_left;
+	array[1] = v_right;
+	array[2] = v_top;
+	array[3] = v_bottom;
+	array[4] = v_near;
+	array[5] = v_far;
+	bool frustumCulled = true;
+	for (int i=0; i<6; i++) {
+		vec4 clipSpaceV = viewProjMat * vec4(array[i].xyz, 1.0);
+		clipSpaceV = clipSpaceV / clipSpaceV.w;
+		bool isInside = (clipSpaceV.x > -1.0) && (clipSpaceV.x < 1.0) && (clipSpaceV.y > -1.0) &&
+		(clipSpaceV.y < 1.0) && (clipSpaceV.z > -1.0) && (clipSpaceV.z < 1.0) ;
+		if (isInside == true) {
+			frustumCulled = false;
+			break;
+		}
+	}
 
-	vec4 clipSpaceV = viewProjMat * vec4(rawInstanceProps[idx].position.xyz, 1.0) ;
-	clipSpaceV = clipSpaceV / clipSpaceV.w;
-	bool frustumCulled = (clipSpaceV.x < -1.0) || (clipSpaceV.x > 1.0) || (clipSpaceV.y < -1.0) ||
-	(clipSpaceV.y > 1.0) || (clipSpaceV.z < -1.0) || (clipSpaceV.z > 1.0) ;
-	
-//	if(true){
-//		const uint UNIQUE_IDX = atomicAdd(commands[0].instanceCount, 1);
-//	    currValidInstanceProps[UNIQUE_IDX] = rawInstanceProps[idx];
-//	}
 	if(frustumCulled == false){
-		int con_n;
-		if (idx>=0 && idx < commands[1].baseInstance) {
-			con_n = 0;
-		}
-		if (idx>=commands[1].baseInstance && idx<commands[2].baseInstance) {
-			con_n = 1;
-		}
-		if (idx>=commands[2].baseInstance && idx<commands[3].baseInstance) {
-			con_n = 2;
-		}
-		if (idx>=commands[3].baseInstance && idx<commands[4].baseInstance) {
-			con_n = 3;
-		}
-		if (idx>=commands[4].baseInstance) {
-			con_n = 4;
-		}
 		const uint UNIQUE_IDX = atomicAdd(commands[con_n].instanceCount, 1);
 	    currValidInstanceProps[UNIQUE_IDX+commands[con_n].baseInstance] = rawInstanceProps[idx] ;
 		validRotCol0[UNIQUE_IDX+commands[con_n].baseInstance] = rawRotCol0[idx] ;
